a3d50de7de45acb101fbb9bc0ad708e5e59a549f
Toua Thao <touathao09@gmail.com>
2019-04-13 10:34:21 -0500

    Lab 2 completed successfully

--- lab-02/src/projects/ProjectRow.js
+++ lab-02/src/projects/ProjectRow.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 class ProjectRow extends React.Component {
-  // TODO Implement
+
+    render() {
+        const { project } = this.props;
+        
+        return (
+          <tr>
+            <td>{project.name}</td>
+            <td>{project.description}</td>
+          </tr>
+        );
+      }
+      
+  
 }
 
+ProjectRow.propTypes = {
+    project: PropTypes.object.isRequired
+  };
 export default ProjectRow;
